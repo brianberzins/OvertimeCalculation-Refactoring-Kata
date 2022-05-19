7d1f807d6c1b4c667cb8238688616964f7bc0a2d
brianberzins <brianberzins@gmail.com>
2022-05-19 18:18:42 -0500

    r - inline variables

--- java/src/main/java/codingdojo/overtimecalculator/NonUnionCalculator.java
+++ java/src/main/java/codingdojo/overtimecalculator/NonUnionCalculator.java
@@ -6,13 +6,12 @@ import codingdojo.Overtime;
 
 public class NonUnionCalculator implements OvertimeCalculator {
 
-    public static final double MAX_HOURS_RATE_1 = 10d;
-
     @Override
     public Overtime calculate(double hours, Assignment assignment, Briefing briefing) {
         if (briefing.foreign() || (!briefing.watcode() && !briefing.z3())) {
             return new Overtime(hours, 0d);
         }
-        return new Overtime(Double.min(hours, MAX_HOURS_RATE_1), hours - Double.min(hours, MAX_HOURS_RATE_1));
+        var maxHoursRate1 = 10d;
+        return new Overtime(Double.min(hours, maxHoursRate1), hours - Double.min(hours, maxHoursRate1));
     }
 }
--- java/src/main/java/codingdojo/overtimecalculator/UnionOvertimeCalculator.java
+++ java/src/main/java/codingdojo/overtimecalculator/UnionOvertimeCalculator.java
@@ -6,20 +6,17 @@ import codingdojo.Overtime;
 
 public class UnionOvertimeCalculator implements OvertimeCalculator {
 
-    public static final double MAX_HOURS_RATE_1 = 10d;
-    public static final double MAX_HOURS_RATE_2 = 6d;
-
     @Override
     public Overtime calculate(double hours, Assignment assignment, Briefing briefing) {
         if (briefing.watcode() || briefing.hbmo()) {
             return new Overtime(hours, 0d);
         }
 
-        return twoRateOvertime(hours, assignment.inHours(), MAX_HOURS_RATE_2);
+        return twoRateOvertime(hours, assignment.inHours(), 6d);
     }
 
     private Overtime twoRateOvertime(double hours, double assignmentHours, double maxHoursRate2) {
-        var hoursRate1 = Double.min(hours, MAX_HOURS_RATE_1);
+        var hoursRate1 = Double.min(hours, 10d);
         var hoursRate2 = Double.min(Double.min(hours - hoursRate1, maxHoursRate2), assignmentHours);
         return new Overtime(hoursRate1, hoursRate2);
     }
