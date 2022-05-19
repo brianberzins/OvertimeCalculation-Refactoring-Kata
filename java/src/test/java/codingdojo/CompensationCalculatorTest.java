0b8f98a5a12e36b405e75e7dc4986bead6a9ff43
brianberzins <brianberzins@gmail.com>
2022-05-19 11:41:24 -0500

    t - added test case

--- java/src/test/java/codingdojo/CompensationCalculatorTest.java
+++ java/src/test/java/codingdojo/CompensationCalculatorTest.java
@@ -2,12 +2,19 @@ package codingdojo;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CompensationCalculatorTest {
+
     @Test
-    void fixme() {
-        assertEquals(true, false);
+    void unionizedWatcode() {
+        var briefing = new Briefing(true, false, false, false);
+        var assignment = new Assignment(true, Duration.ofHours(8));
+        var overtime = CompensationCalculator.calculateOvertime(new BigDecimal("5.0"), assignment, briefing);
+        assertEquals(new Overtime(new BigDecimal("5.0"), BigDecimal.ZERO), overtime);
     }
 
 }
