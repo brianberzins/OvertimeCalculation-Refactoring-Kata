628a8fdb8ab53fad84ef6ec7dfdce8d894cc4759
brianberzins <brianberzins@gmail.com>
2022-05-19 15:22:31 -0500

    t - include additional test cases

--- java/src/test/java/codingdojo/CompensationCalculatorTest.java
+++ java/src/test/java/codingdojo/CompensationCalculatorTest.java
@@ -11,7 +11,7 @@ public class CompensationCalculatorTest {
 
     @Test
     void combinations() {
-        BigDecimal[] overtimes = Queryable.as(-1.0, 8.0, 12.0).select(BigDecimal::new).asArray();
+        BigDecimal[] overtimes = Queryable.as(-1.0, 8.0, 12.0, 20.0).select(BigDecimal::new).asArray();
         Duration[] durations =  Queryable.as(5, 10).select(Duration::ofHours).asArray();
         Boolean[] booleans = {true, false};
         CombinationApprovals.verifyAllCombinations((a, b, c, d, e, f, g) -> {
