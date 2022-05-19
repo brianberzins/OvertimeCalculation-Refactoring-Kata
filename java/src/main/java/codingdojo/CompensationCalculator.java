227bb4f1d50c2f76c37adff77e63f180d3e97dea
brianberzins <brianberzins@gmail.com>
2022-05-19 16:06:17 -0500

    r - inline variables

--- java/src/main/java/codingdojo/CompensationCalculator.java
+++ java/src/main/java/codingdojo/CompensationCalculator.java
@@ -6,9 +6,6 @@ import java.math.BigDecimal;
 
 public class CompensationCalculator {
 
-    public final static BigDecimal MAX_OVERTIME_HOURS_RATE_1 = BigDecimal.TEN;
-    public static final int THRESHOLD_OVERTIME_HOURS_RATE_2 = 6;
-
     public static Overtime calculateOvertime(BigDecimal hoursOvertime, Assignment assignment, Briefing briefing) {
         var u = assignment.isUnionized();
         var w = briefing.watcode();
@@ -31,12 +28,12 @@ public class CompensationCalculator {
             return singleRateOvertime(hoursOvertime);
         } else if (hoursOvertime.compareTo(BigDecimal.ZERO) < 1) {
             return singleRateOvertime(BigDecimal.ZERO);
-        } else if (hoursOvertime.compareTo(MAX_OVERTIME_HOURS_RATE_1) < 1) {
+        } else if (hoursOvertime.compareTo(BigDecimal.TEN) < 1) {
             return singleRateOvertime(hoursOvertime);
-        } else if (u && assignment.duration().minusHours(THRESHOLD_OVERTIME_HOURS_RATE_2).isNegative()) {
+        } else if (u && assignment.duration().minusHours(6).isNegative()) {
             return twoRateOvertime(hoursOvertime, BigDecimal.valueOf(assignment.duration().toSeconds() / 3600));
-        } else if (u && !(assignment.duration().minusHours(THRESHOLD_OVERTIME_HOURS_RATE_2).isNegative())) {
-            return twoRateOvertime(hoursOvertime, BigDecimal.valueOf(THRESHOLD_OVERTIME_HOURS_RATE_2));
+        } else if (u && !(assignment.duration().minusHours(6).isNegative())) {
+            return twoRateOvertime(hoursOvertime, BigDecimal.valueOf(6));
         } else {
             return twoRateOvertime(hoursOvertime);
         }
@@ -47,7 +44,7 @@ public class CompensationCalculator {
     }
 
     private static Overtime twoRateOvertime(BigDecimal hoursOvertime, BigDecimal maximumDoubleOvertime) {
-        return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertime.subtract(MAX_OVERTIME_HOURS_RATE_1).min(maximumDoubleOvertime));
+        return new Overtime(BigDecimal.TEN, hoursOvertime.subtract(BigDecimal.TEN).min(maximumDoubleOvertime));
     }
 
     private static Overtime singleRateOvertime(BigDecimal hoursOvertimeTotal) {
