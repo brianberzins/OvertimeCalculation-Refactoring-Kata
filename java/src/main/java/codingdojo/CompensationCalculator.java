490af229f04ca3af4da25dbba34476bde6cdd50e
brianberzins <brianberzins@gmail.com>
2022-05-19 17:24:17 -0500

    R - collapse unused

--- java/src/main/java/codingdojo/CompensationCalculator.java
+++ java/src/main/java/codingdojo/CompensationCalculator.java
@@ -10,22 +10,8 @@ public class CompensationCalculator {
 
     public static Overtime calculateOvertime(BigDecimal hoursOvertime, Assignment assignment, Briefing briefing) {
         hoursOvertime = BigDecimal.ZERO.max(hoursOvertime);
-        OvertimeCalculator[] overtimeCalculators = {
-                new UnionOvertimeCalculator(),
-                new NonUnionCalculator(),
-        };
-        for (OvertimeCalculator overtimeCalculator : overtimeCalculators) {
-            if (overtimeCalculator.appliesTo(hoursOvertime, assignment, briefing)) {
-                return overtimeCalculator.calculate(hoursOvertime, assignment, briefing);
-            }
-        }
-        throw new RuntimeException("unhandled overtime calculation");
-    }
-
-    private static Overtime overtime(BigDecimal hours, BigDecimal maxRate1Overtime, BigDecimal maxRate2Overtime) {
-        var hoursRate1 = BigDecimal.ZERO.max(hours).min(maxRate1Overtime);
-        var hoursRate2 = hours.subtract(hoursRate1).min(maxRate2Overtime);
-        return new Overtime(hoursRate1, hoursRate2);
+        OvertimeCalculator overtimeCalculator = assignment.isUnionized() ? new UnionOvertimeCalculator() : new NonUnionCalculator();
+        return overtimeCalculator.calculate(hoursOvertime, assignment, briefing);
     }
 
 }
