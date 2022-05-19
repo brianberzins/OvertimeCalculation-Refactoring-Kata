af658f75833c4f43ca33160a6b330c95ae6c7b44
brianberzins <brianberzins@gmail.com>
2022-05-19 12:09:58 -0500

    t - remove auto approval

--- java/src/test/java/codingdojo/CompensationCalculatorTest.java
+++ java/src/test/java/codingdojo/CompensationCalculatorTest.java
@@ -1,8 +1,6 @@
 package codingdojo;
 
 import org.approvaltests.combinations.CombinationApprovals;
-import org.approvaltests.reporters.AutoApproveReporter;
-import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
 import org.lambda.query.Queryable;
 
@@ -12,17 +10,15 @@ import java.time.Duration;
 public class CompensationCalculatorTest {
 
     @Test
-    @UseReporter(AutoApproveReporter.class)
     void combinations() {
-        BigDecimal[] overtimes = Queryable.as(8.0).select(BigDecimal::new).asArray();
-        Duration[] durations =  Queryable.as(10).select(Duration::ofHours).asArray();
+        BigDecimal[] overtimes = Queryable.as(-1.0, 8.0, 12.0).select(BigDecimal::new).asArray();
+        Duration[] durations =  Queryable.as(5, 10).select(Duration::ofHours).asArray();
         Boolean[] booleans = {true, false};
         CombinationApprovals.verifyAllCombinations((a, b, c, d, e, f, g) -> {
             var assignment = new Assignment(b, c);
             var briefing = new Briefing(d, e, f, g);
             return CompensationCalculator.calculateOvertime(a, assignment, briefing);
         }, overtimes, booleans, durations, booleans, booleans, booleans, booleans);
-
     }
 
 }
