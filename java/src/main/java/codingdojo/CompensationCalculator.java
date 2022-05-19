f6e715728901d21b091ebd3e6389c8037fd32b64
brianberzins <brianberzins@gmail.com>
2022-05-19 12:28:43 -0500

    r - inline variables

--- java/src/main/java/codingdojo/CompensationCalculator.java
+++ java/src/main/java/codingdojo/CompensationCalculator.java
@@ -16,32 +16,27 @@ public class CompensationCalculator {
                         || (briefing.foreign() && !assignment.isUnionized())
         ) {
             BigDecimal hoursOvertimeRate2 = BigDecimal.ZERO;
-            BigDecimal hoursOvertimeRate1 = hoursOvertimeTotal;
-            return new Overtime(hoursOvertimeRate1, hoursOvertimeRate2);
+            return new Overtime(hoursOvertimeTotal, hoursOvertimeRate2);
         } else if (hoursOvertimeTotal.compareTo(BigDecimal.ZERO) < 1) {
             BigDecimal hoursOvertimeRate1 = BigDecimal.ZERO;
             BigDecimal hoursOvertimeRate2 = BigDecimal.ZERO;
             return new Overtime(hoursOvertimeRate1, hoursOvertimeRate2);
         } else if (hoursOvertimeTotal.compareTo(MAX_OVERTIME_HOURS_RATE_1) < 1) {
             BigDecimal hoursOvertimeRate2 = BigDecimal.ZERO;
-            BigDecimal hoursOvertimeRate1 = hoursOvertimeTotal;
-            return new Overtime(hoursOvertimeRate1, hoursOvertimeRate2);
+            return new Overtime(hoursOvertimeTotal, hoursOvertimeRate2);
         } else if (assignment.isUnionized() && assignment.duration().minusHours(THRESHOLD_OVERTIME_HOURS_RATE_2).isNegative()) {
-            BigDecimal hoursOvertimeRate1 = MAX_OVERTIME_HOURS_RATE_1;
             BigDecimal hoursOvertimeRate2 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1);
             BigDecimal threshold = BigDecimal.valueOf(assignment.duration().toSeconds() / 3600);
             hoursOvertimeRate2 = hoursOvertimeRate2.min(threshold);
-            return new Overtime(hoursOvertimeRate1, hoursOvertimeRate2);
+            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeRate2);
         } else if (assignment.isUnionized() && !(assignment.duration().minusHours(THRESHOLD_OVERTIME_HOURS_RATE_2).isNegative())) {
-            BigDecimal hoursOvertimeRate1 = MAX_OVERTIME_HOURS_RATE_1;
             BigDecimal hoursOvertimeRate2 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1);
             BigDecimal threshold = BigDecimal.valueOf(THRESHOLD_OVERTIME_HOURS_RATE_2);
             hoursOvertimeRate2 = hoursOvertimeRate2.min(threshold);
-            return new Overtime(hoursOvertimeRate1, hoursOvertimeRate2);
+            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeRate2);
         } else {
-            BigDecimal hoursOvertimeRate1 = MAX_OVERTIME_HOURS_RATE_1;
             BigDecimal hoursOvertimeRate2 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1);
-            return new Overtime(hoursOvertimeRate1, hoursOvertimeRate2);
+            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeRate2);
         }
     }
 
