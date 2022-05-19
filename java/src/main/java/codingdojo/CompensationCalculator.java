2e7155259de1987f93b98102769de6dc8f462f32
brianberzins <brianberzins@gmail.com>
2022-05-19 15:33:35 -0500

    r - introduce variables, combine cases

--- java/src/main/java/codingdojo/CompensationCalculator.java
+++ java/src/main/java/codingdojo/CompensationCalculator.java
@@ -8,23 +8,22 @@ public class CompensationCalculator {
     public static final int THRESHOLD_OVERTIME_HOURS_RATE_2 = 6;
 
     public static Overtime calculateOvertime(BigDecimal hoursOvertime, Assignment assignment, Briefing briefing) {
-        if (!briefing.watcode() && !briefing.z3() && !assignment.isUnionized()) {
-            return singleRateOvertime(hoursOvertime);
-        } else if (briefing.hbmo() && assignment.isUnionized()) {
-            return singleRateOvertime(hoursOvertime);
-        } else if (briefing.watcode() && !assignment.isUnionized() && briefing.foreign()) {
-            return singleRateOvertime(hoursOvertime);
-        } else if (briefing.watcode() && assignment.isUnionized()) {
-            return singleRateOvertime(hoursOvertime);
-        } else if (briefing.foreign() && !assignment.isUnionized()) {
+        var u = assignment.isUnionized();
+        var w = briefing.watcode();
+        var z = briefing.z3();
+        var f = briefing.foreign();
+        var h = briefing.hbmo();
+
+
+        if ((!w && !z && !u) || (h && u) || (w && !u && f) || (w && u) || (f && !u)){
             return singleRateOvertime(hoursOvertime);
         } else if (hoursOvertime.compareTo(BigDecimal.ZERO) < 1) {
             return singleRateOvertime(BigDecimal.ZERO);
         } else if (hoursOvertime.compareTo(MAX_OVERTIME_HOURS_RATE_1) < 1) {
             return singleRateOvertime(hoursOvertime);
-        } else if (assignment.isUnionized() && assignment.duration().minusHours(THRESHOLD_OVERTIME_HOURS_RATE_2).isNegative()) {
+        } else if (u && assignment.duration().minusHours(THRESHOLD_OVERTIME_HOURS_RATE_2).isNegative()) {
             return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertime.subtract(MAX_OVERTIME_HOURS_RATE_1).min(BigDecimal.valueOf(assignment.duration().toSeconds() / 3600)));
-        } else if (assignment.isUnionized() && !(assignment.duration().minusHours(THRESHOLD_OVERTIME_HOURS_RATE_2).isNegative())) {
+        } else if (u && !(assignment.duration().minusHours(THRESHOLD_OVERTIME_HOURS_RATE_2).isNegative())) {
             return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertime.subtract(MAX_OVERTIME_HOURS_RATE_1).min(BigDecimal.valueOf(THRESHOLD_OVERTIME_HOURS_RATE_2)));
         } else {
             return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertime.subtract(MAX_OVERTIME_HOURS_RATE_1));
