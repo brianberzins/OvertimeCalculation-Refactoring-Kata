ddfff00c87eea2187febf0bde5691fdd479d4e53
brianberzins <brianberzins@gmail.com>
2022-05-19 11:55:26 -0500

    t - combination approvals, target first condition

--- java/src/test/java/codingdojo/CompensationCalculatorTest.java
+++ java/src/test/java/codingdojo/CompensationCalculatorTest.java
@@ -1,20 +1,25 @@
 package codingdojo;
 
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.reporters.AutoApproveReporter;
+import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
+import org.lambda.query.Queryable;
 
 import java.math.BigDecimal;
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class CompensationCalculatorTest {
 
+
+
     @Test
-    void unionizedWatcode() {
-        var briefing = new Briefing(true, false, false, false);
-        var assignment = new Assignment(true, Duration.ofHours(8));
-        var overtime = CompensationCalculator.calculateOvertime(new BigDecimal("5.0"), assignment, briefing);
-        assertEquals(new Overtime(new BigDecimal("5.0"), BigDecimal.ZERO), overtime);
+    @UseReporter(AutoApproveReporter.class)
+    void combinations() {
+        BigDecimal[] a = Queryable.as(8.0).select(BigDecimal::new).asArray();
+        Assignment[] b = {new Assignment(false, Duration.ofHours(10))};
+        Briefing[] c = {new Briefing(false, false, true, true)};
+        CombinationApprovals.verifyAllCombinations(CompensationCalculator::calculateOvertime, a, b, c);
     }
 
 }
