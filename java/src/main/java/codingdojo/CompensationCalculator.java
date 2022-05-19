113130d887883cd78aba9d295a119efa5226c3ed
brianberzins <brianberzins@gmail.com>
2022-05-19 12:37:55 -0500

    r - inline variables

--- java/src/main/java/codingdojo/CompensationCalculator.java
+++ java/src/main/java/codingdojo/CompensationCalculator.java
@@ -23,18 +23,11 @@ public class CompensationCalculator {
         } else if (hoursOvertimeTotal.compareTo(MAX_OVERTIME_HOURS_RATE_1) < 1) {
             return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
         } else if (assignment.isUnionized() && assignment.duration().minusHours(THRESHOLD_OVERTIME_HOURS_RATE_2).isNegative()) {
-            BigDecimal hoursOvertimeRate2 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1);
-            BigDecimal threshold = BigDecimal.valueOf(assignment.duration().toSeconds() / 3600);
-            hoursOvertimeRate2 = hoursOvertimeRate2.min(threshold);
-            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeRate2);
+            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1).min(BigDecimal.valueOf(assignment.duration().toSeconds() / 3600)));
         } else if (assignment.isUnionized() && !(assignment.duration().minusHours(THRESHOLD_OVERTIME_HOURS_RATE_2).isNegative())) {
-            BigDecimal hoursOvertimeRate2 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1);
-            BigDecimal threshold = BigDecimal.valueOf(THRESHOLD_OVERTIME_HOURS_RATE_2);
-            hoursOvertimeRate2 = hoursOvertimeRate2.min(threshold);
-            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeRate2);
+            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1).min(BigDecimal.valueOf(THRESHOLD_OVERTIME_HOURS_RATE_2)));
         } else {
-            BigDecimal hoursOvertimeRate2 = hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1);
-            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeRate2);
+            return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1));
         }
     }
 
