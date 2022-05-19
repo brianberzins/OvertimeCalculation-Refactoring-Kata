0bf5b1c83033c6d9802e508508e37b9b488c356e
brianberzins <brianberzins@gmail.com>
2022-05-19 12:41:02 -0500

    r - extract method

--- java/src/main/java/codingdojo/CompensationCalculator.java
+++ java/src/main/java/codingdojo/CompensationCalculator.java
@@ -9,19 +9,19 @@ public class CompensationCalculator {
 
     public static Overtime calculateOvertime(BigDecimal hoursOvertimeTotal, Assignment assignment, Briefing briefing) {
         if (!briefing.watcode() && !briefing.z3() && !assignment.isUnionized()) {
-            return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
+            return singleRateOvertime(hoursOvertimeTotal);
         } else if (briefing.hbmo() && assignment.isUnionized()) {
-            return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
+            return singleRateOvertime(hoursOvertimeTotal);
         } else if (briefing.watcode() && !assignment.isUnionized() && briefing.foreign()) {
-            return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
+            return singleRateOvertime(hoursOvertimeTotal);
         } else if (briefing.watcode() && assignment.isUnionized()) {
-            return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
+            return singleRateOvertime(hoursOvertimeTotal);
         } else if (briefing.foreign() && !assignment.isUnionized()) {
-            return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
+            return singleRateOvertime(hoursOvertimeTotal);
         } else if (hoursOvertimeTotal.compareTo(BigDecimal.ZERO) < 1) {
-            return new Overtime(BigDecimal.ZERO, BigDecimal.ZERO);
+            return singleRateOvertime(BigDecimal.ZERO);
         } else if (hoursOvertimeTotal.compareTo(MAX_OVERTIME_HOURS_RATE_1) < 1) {
-            return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
+            return singleRateOvertime(hoursOvertimeTotal);
         } else if (assignment.isUnionized() && assignment.duration().minusHours(THRESHOLD_OVERTIME_HOURS_RATE_2).isNegative()) {
             return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1).min(BigDecimal.valueOf(assignment.duration().toSeconds() / 3600)));
         } else if (assignment.isUnionized() && !(assignment.duration().minusHours(THRESHOLD_OVERTIME_HOURS_RATE_2).isNegative())) {
@@ -29,6 +29,10 @@ public class CompensationCalculator {
         } else {
             return new Overtime(MAX_OVERTIME_HOURS_RATE_1, hoursOvertimeTotal.subtract(MAX_OVERTIME_HOURS_RATE_1));
         }
+    }
+
+    private static Overtime singleRateOvertime(BigDecimal hoursOvertimeTotal) {
+        return new Overtime(hoursOvertimeTotal, BigDecimal.ZERO);
     }
 
 }
